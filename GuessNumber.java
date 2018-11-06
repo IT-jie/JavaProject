8dd73912446deecf66ff7f717d904377ac57113f
IT-jie <1793208105@qq.com>
2018-11-06 11:35:55 +0800

    Create

---
+++ GuessNumber.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class GuessNumber{
+	public static void main(String[] args){
+		Random random = new Random();//创建random
+		int x = random.nextInt(100)+1;//生成一个1-100之间的随机数 random.nextInt(100)的范围为0-99，所以+1，范围为1-100
+		System.out.println("系统已自动为您生成了一个随机数（范围为1-100），游戏开始！");
+		System.out.print("猜猜这个数字是多少吧：");
+		Scanner in = new Scanner(System.in);//创建scanner
+		int y = in.nextInt(); //输入数字
+		int count = 1; //次数
+		
+		
+		while(y != x){
+			
+			count ++;
+			
+			if(y<1 || y>100){
+			System.out.print("Sorry,你猜的数字不在范围之内（范围为1-100），请再重新猜一次吧：");
+			y = in.nextInt();
+			}
+			
+			else if (y>x){
+				System.out.print("Sorry,你输入的数字太大了，请再重新猜一次吧：");
+				y = in.nextInt();
+ 
+			}
+			else if (y<x){
+				System.out.print("Sorry,你输入的数字太小了，请再重新猜一次吧：");
+				y = in.nextInt();
+			}
+		}
+		System.out.println("恭喜你，猜对了！你猜的数字是" + y + "你总共猜了" + count + "次！");
+	}
+}
---
+++ RockPaperScissors.java
@@ -0,0 +1,101 @@
+import java.util.*;
+
+class Person {
+    public String name;
+    public int score;
+
+    public Person(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String play(int i) {
+        return i == 1?"剪刀":(i == 2?"石头":"布");
+    }
+}
+
+public class RockPaperScissors {
+	
+	public static void main(String[] args) {
+
+		int num = 0;
+
+        System.out.println("*****************\n****猜拳, 开始****\n*****************");
+        System.out.println("出拳规则:1.剪刀 2.石头 3.布");
+        System.out.println("请选择对方角色<1:刘备 2:孙权 3:曹操>");
+
+        Scanner scanner = new Scanner(System.in);
+		
+		//new一个对手
+		Person opponent = new Person("",0);
+        int nameIndex = scanner.nextInt();
+        while (true){
+            if (nameIndex == 1){
+                opponent.name = "刘备";
+                break;
+            }else if (nameIndex == 2){
+                opponent.name  = "孙权";
+                break;
+            }else if (nameIndex == 3){
+                opponent.name = "曹操";
+                break;
+            }else {
+                System.out.println("输入错误!输入对方角色<1:刘备 2:孙权 3:曹操>");
+                nameIndex = scanner.nextInt();
+            }
+        }
+
+		System.out.println("请输入您的姓名:");
+        Scanner scanner1 = new Scanner(System.in);
+        Person self = new Person(scanner1.next(),0);
+        System.out.println(self.name + " VS " +opponent.name);
+        System.out.println("要开始游戏吗?<y/n>");
+
+        Scanner scanner2 = new Scanner(System.in);
+        String option = scanner2.next();
+        while (option.equals("y")){
+            System.out.println("请出拳:1、剪刀 2、石头 3、布");
+            Scanner scanner3 = new Scanner(System.in);
+            int select = scanner3.nextInt();
+            String result1;
+            if (select!=1&&select!=2&&select!=3){
+                System.out.println("输入错误!请输入出拳规则:1、剪刀 2、石头 3、布");
+                select = scanner3.nextInt();
+            }
+            result1 = self.play(select);
+            System.out.println(self.name+"出:"+result1);
+            String result2 = opponent.play(new Random().nextInt(3)+1);
+            System.out.println(opponent.name+"出:"+result2);
+
+
+            if (result1.equals(result2)){
+                System.out.println("和局");
+            }else if (result1.equals("剪刀")&&result2.equals("布")||result1.equals("石头")&&result2.equals("剪刀")||result1.equals("布")&&result2.equals("石头")){
+                System.out.println("你赢了! "+self.name+"赢!");
+                self.score += 1;
+            }else {
+                System.out.println("你输了! "+opponent.name+"赢!");
+                opponent.score += 1;
+            }
+            num++;
+            System.out.println("需要进行下一局吗?<y/n>");
+            option = scanner2.next();
+            if (option.equals("n")){
+                System.out.println("=================");
+                System.out.println("共进行"+num+"局\n"+self.name+"得分为"+self.score+","+opponent.name+"得分为"+opponent.score +"\n");
+                if (self.score>opponent.score){
+                    System.out.println(self.name+"赢!"+self.name+"好棒!");
+                }else if (self.score == opponent.score){
+                    System.out.println("平局!");
+                }else {
+                    System.out.println(opponent.name+"赢!"+self.name+"笨蛋!");
+                }
+                System.out.println("感谢你的使用!");
+            }else if (!option.equals("y")){
+                System.out.println("输入错误,请重新输入!");
+                System.out.println("需要进行下一局吗?<y/n>");
+                option = scanner2.next();
+            }
+		}
+	}
+}
